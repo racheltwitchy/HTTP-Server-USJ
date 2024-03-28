8fe02ab94b6a80bc4a63f8bdc80ca94b80e7939b
racheltwitchy <raquel.pelaire@gmail.com>
2024-03-28 12:28:24 +0100

    Change classes name

--- client.java
+++ client.java
@@ -3,22 +3,22 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class MyHTTPClient {
+public class client {
     public static void main(String[] args) {
         String server = "example.com";
         int port = 80;
-        
+
         String request = "GET / HTTP/1.1\r\n"
-                       + "Host: " + server + "\r\n"
-                       + "Connection: close\r\n"
-                       + "\r\n";
-        
+                + "Host: " + server + "\r\n"
+                + "Connection: close\r\n"
+                + "\r\n";
+
         try (Socket socket = new Socket(server, port);
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-            
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
             out.println(request);
-            
+
             String line;
             while ((line = in.readLine()) != null) {
                 System.out.println(line);
