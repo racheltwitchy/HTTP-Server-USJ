1ecd445056c4106add7958f91e0d6089cfa486cd
Frodini <151853364+Frodini@users.noreply.github.com>
2024-04-03 22:27:26 +0200

    Modified to adapte the server to class Car

--- server.java
+++ server.java
@@ -6,15 +6,16 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
-public class server {
+public class Server {
 
     public static void main(String[] args) {
-        int port = 3000;
-        
-        ArrayList<String> messages = new ArrayList<String>();
+        ArrayList<Car> coches = new ArrayList<Car>();
         String response = "";
+
+        System.out.println("Type the port you want: ");
+        String port = System.console().readLine();
         
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
+        try (ServerSocket serverSocket = new ServerSocket(Integer.parseInt(port))) {
             System.out.println("Server is listening on port " + port);
             
             while (true) {
@@ -34,7 +35,6 @@ public class server {
                     String request = requestBuilder.toString();
                     String method = request.split(" ")[0];
                     //int bodyIndex = request.indexOf("\r\n\r\n"); Doesnt work properly
-
                     //String body = request.substring(bodyIndex + 4); // Skip the "\r\n\r\n"
                     String body=request.split("\r\n")[request.split("\r\n").length-1];
                     if(method.equals("GET")){
@@ -42,48 +42,58 @@ public class server {
                         "Echoing back your request body(GET):\r\n" 
                         + "Content-Length: "+ body.getBytes().length +" \r\n"
                         + "Date: "+ java.time.LocalDateTime.now() + "\r\n"
-                        + body
+                        + coches.toString()
                         + "\r\n";
                     }
                     if(method.equals("HEAD")){
                         response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\n"
                         + "Date: "+ java.time.LocalDateTime.now() + "\r\n";
-                       
                     }
                     if(method.equals("PUT")){
-                        if(!messages.contains(body)){
-                            messages.add(body);
+                        String[] parts = body.split(" ");
+                        Car temp=new Car(parts[0], parts[1], parts[2], Integer.parseInt(parts[3]));
+                        
+                        if(!coches.contains(temp)){
+                            coches.add(temp);
                             response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\n"
                             + "Date: "+ java.time.LocalDateTime.now() + "\r\n"
-                            + "Added file: " + body ;
+                            + "Added car: " + temp.toString() ;
                         }else{
                             response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\n"
                             + "Date: "+ java.time.LocalDateTime.now() + "\r\n" 
-                            + "The file already exists";
+                            + "The car already exists";
                         }
                     }
                     if(method.equals("POST")){
                         String[] numbers = body.split(" ");
-                        int sum = Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+                        double min=Double.min(Double.parseDouble(numbers[0]),Double.parseDouble(numbers[1]));
+                        double max=Double.max(Double.parseDouble(numbers[0]),Double.parseDouble(numbers[1]));
+                        ArrayList<Car> temp = new ArrayList<Car>();
+                        for(Car c: coches){
+                            if(c.price>=min && c.price<=max){
+                                temp.add(c);
+                            }
+                        }
                         response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\n" 
                         + "Date: "+ java.time.LocalDateTime.now() + "\r\n"
-                        + "Sum: " + sum;
-                        
+                        + "The car between the prices (" + min + " y " + max + ") are: \r\n"
+                        + temp.toString();
                     }
                     if(method.equals("DELETE")){
-                        if(messages.contains(body)){
-                            messages.remove(body);
+                        String[] parts = body.split(" ");
+                        Car temp=new Car(parts[0], parts[1], parts[2], Integer.parseInt(parts[3]));
+                        if(coches.contains(temp)){
+                            coches.remove(temp);
                             response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\n" 
                             + "Date: "+ java.time.LocalDateTime.now() + "\r\n"
-                            + "Deleted file: " + body;
+                            + "Deleted car: " + temp.toString();
                         }else{
                             response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\n" 
                             + "Date: "+ java.time.LocalDateTime.now() + "\r\n"
-                            + "The file does not exist";
+                            + "The car does not exist";
                         }
                     }
-                    writer.print(response);
-                   
+                    writer.print(response); 
                 }
             }
         } catch (Exception e) {
