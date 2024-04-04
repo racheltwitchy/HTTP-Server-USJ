7165befcbcc692f76190f070331e2265cce8a897
Frodini <151853364+Frodini@users.noreply.github.com>
2024-04-04 19:16:51 +0200

    Change variable, fixed method equals and implemented hashCode

--- Car.java
+++ Car.java
@@ -1,21 +1,38 @@
 public class Car{
     String brand;
     String model;
-    String horsePower;
+    int horsePower;
     int price;
 
-    public  Car(String brand, String model, String horsePower, int price) {
+    public  Car(String brand, String model, int horsePower, int price) {
         this.brand = brand;
         this.model = model;
         this.horsePower = horsePower;
         this.price = price;
     }
 
-    public boolean equals(Car c){
-        return this.brand.equals(c.brand) && this.model.equals(c.model) && this.horsePower.equals(c.horsePower) ;       
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Car other = (Car) obj;
+        return this.brand.equals(other.brand) && 
+               this.model.equals(other.model) && 
+               this.horsePower == other.horsePower &&
+               this.price == other.price;
     }
 
+    @Override
+    public int hashCode() {
+        int result = brand != null ? brand.hashCode() : 0;
+        result = 31 * result + (model != null ? model.hashCode() : 0);
+        result = 31 * result + horsePower;
+        result = 31 * result + price;
+        return result;
+    }
+
+    
     public String toString(){
-        return "\r\nBrand: " + brand + " Model: " + model + " Horse Power: " + horsePower + " Price: " + price +"\r\n";      
+        return " Brand: " + brand + " Model: " + model + " Horse Power: " + horsePower + " Price: " + price +"\r\n";      
     }
 }
