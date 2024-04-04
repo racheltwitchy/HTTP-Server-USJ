3b302fa76ddb25755e1a0ce581cce38bd98d5383
racheltwitchy <raquel.pelaire@gmail.com>
2024-04-04 22:01:46 +0200

    Update client.java

--- client.java
+++ client.java
@@ -10,47 +10,49 @@ public class Client {
         String server = System.console().readLine();
         System.out.println("Type the port you want to connect to: ");
         String port = System.console().readLine();
-        String method="";
-        String body = ""; 
-        do{
-            do{
+        String method = "";
+        String body = "";
+        do {
+            do {
                 System.out.println("Type the HTTP method you want to use (GET, HEAD, PUT, POST, DELETE or EXIT): ");
                 method = System.console().readLine();
-            }while(!method.equals("GET") && !method.equals("HEAD") && !method.equals("PUT") && !method.equals("POST") && !method.equals("DELETE") && !method.equals("EXIT"));
-            
-            if(method.equals("GET") || method.equals("HEAD")){
+            } while (!method.equals("GET") && !method.equals("HEAD") && !method.equals("PUT") && !method.equals("POST")
+                    && !method.equals("DELETE") && !method.equals("EXIT"));
+
+            if (method.equals("GET") || method.equals("HEAD")) {
                 System.out.println("Type the body you want to add, or press enter   : ");
                 body = System.console().readLine();
             }
-            if(method.equals("PUT") || method.equals("DELETE")){
-                if(method.equals("PUT")){
-                    System.out.println("Type the car you want to add (Brand Model HorsePower Price) just leave space between each characteristic: ");
+            if (method.equals("PUT") || method.equals("DELETE")) {
+                if (method.equals("PUT")) {
+                    System.out.println(
+                            "Type the car you want to add (Brand Model HorsePower Price) just leave space between each characteristic: ");
                     body = System.console().readLine();
                 }
-                if(method.equals("DELETE")){
+                if (method.equals("DELETE")) {
                     System.out.println("Type the index of the car you want to delete: ");
                     body = System.console().readLine();
                 }
             }
-            if(method.equals("POST")){
+            if (method.equals("POST")) {
                 System.out.println("Type two prices (float) spaced by a space to search cars: ");
                 body = System.console().readLine();
             }
-            
+
             String request = method + " / HTTP/1.1\r\n"
-                        + "Host: " + server + "\r\n"
-                        + "Connection: close\r\n" 
-                        + "Content-Length: "+ body.getBytes().length +" \r\n"
-                        + "Date: "+ java.time.LocalDateTime.now() + "\r\n"
-                        + body+ "\r\n";
-            
+                    + "Host: " + server + "\r\n"
+                    + "Connection: close\r\n"
+                    + "Content-Length: " + body.getBytes().length + " \r\n"
+                    + "Date: " + java.time.LocalDateTime.now() + "\r\n"
+                    + body + "\r\n";
+
             try (Socket socket = new Socket(server, Integer.parseInt(port));
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
                 out.println(request);
                 System.out.println("Request sent to the server\n " + request);
-                
+
                 String line;
                 while ((line = in.readLine()) != null) {
                     System.out.println(line);
@@ -58,7 +60,6 @@ public class Client {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        }while(!method.equals("EXIT"));
+        } while (!method.equals("EXIT"));
     }
 }
-
